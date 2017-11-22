5ad2dbfb942d4b933649d68697d818ee18cfe7ea
jzane <julianzane1@gmail.com>
2017-11-21 19:56:36 -0500

    Jacobian
    Calcualted jacobian function

--- jacobPRP.m
+++ jacobPRP.m
@@ -1,11 +1,26 @@
 
-function J = jacobPRP(q, L2)
+function [Jv, Jw, J] = jacobPRP(q)
 % Returns the full jacobian matrix of the PRP manipualtor
-    J = [0, -cos(q(2))*(L2 + q(3)), -sin(q(2)); 
-         0, -sin(q(2))*(L2 + q(3)), cos(q(2));
-         1, 0, 0;
-         0, 0, 0;
-         0, 0, 0;
-         0, 1, 0];
+[T01, T02, T03] = fkinPRP(q);
+
+% origins and Z vectors;
+O0 = [0; 0; 0];
+O1 = T01(1:3, 4);
+O2 = T02(1:3, 4);
+O3 = T03(1:3, 4);
+
+Z0 = [0; 0; 1];
+Z1 = T01(1:3, 3);
+Z2 = T02(1:3, 3);
+Z3 = T03(1:3, 3);
+
+Zcross1 = cross(Z1, O3-O1);
+
+    J = [Z0,Zcross1, Z2; 
+         [0, 0, 0], Z1, [0, 0, 0]];
+    Jv = J(1:3, :);
+    Jw = J(4:6, :);
+    
+    % returning the jacobian, linear, and rotational sub-jacobians
 
 end
